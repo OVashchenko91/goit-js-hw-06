95a4765b421149cca6d715742fdf0b98c81e030c
Oksana <oksana.sydorenko0691@icloud.com>
2023-02-05 21:46:04 +0200

    hw6

---
+++ js/task-04.js
@@ -0,0 +1,5 @@
+let counterValue = 0;
+const decBtn = document.querySelector('.counter button[data-action="decrement"]');
+const incBtn = document.querySelector('.counter button[data-action="increment"]');
+const val = document.querySelector("#value");
+
